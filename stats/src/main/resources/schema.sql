1e79a7773cd611d65d055e4ee446d0f0ac4ff222
oxana.khvatova <oxana.khvatova@yandex.ru>
2022-09-27 22:22:59 +0300

    postgres

--- stats/src/main/resources/schema.sql
+++ stats/src/main/resources/schema.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS endpoint_hit
     app  VARCHAR(255)                             NOT NULL,
     uri  VARCHAR(250)                             NOT NULL,
     ip   VARCHAR(250)                             NOT NULL,
-    CONSTRAINT pk_endpoint_hit PRIMARY KEY (id),
-    );
+    CONSTRAINT pk_endpoint_hit PRIMARY KEY (id)
+);
