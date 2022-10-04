d7ce16676694483aa4e3dbce9c523a58de700f5b
oxana.khvatova <oxana.khvatova@yandex.ru>
2022-10-04 21:27:49 +0300

    review fixes 2

--- stats/src/main/java/ru/practicum/explorewhithme/AppConstants.java
+++ stats/src/main/java/ru/practicum/explorewhithme/AppConstants.java
@@ -1,5 +1,5 @@
 package ru.practicum.explorewhithme;
 
 public class AppConstants {
-    final static public String dateFormat  = "yyyy-MM-dd HH:mm:ss";
+    public static final String dateFormat  = "yyyy-MM-dd HH:mm:ss";
 }
